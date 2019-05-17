410de497d6eed6a0ad2b8c6e59b526db16fb990d
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-17 17:57:47 -0300

    adicionando arquivo para requisição (loginRequest)

--- src/loginRequest.js
+++ src/loginRequest.js
@@ -1,18 +1,21 @@
-var email = document.getElementById('txtEmail');
-var pass = document.getElementById('txtSenha');
+function request() {
+    var email = document.getElementById('txtEmail');
+    var pass = document.getElementById('txtSenha');
 
-const parameters = { 'email': email, 'senha': pass };
+    const parameters = { 'email': email, 'senha': pass };
 
-$.ajax({
-    method: "POST",
-    url: "localhost:8080/login",
-    data: JSON.parse(parameters)
-}).done(function (msg) {
-    console.log(msg);
-})
-    .fail(function (jqXHR, textStatus, msg) {
-        alert(msg);
-});
+    $.ajax({
+        method: "POST",
+        url: "localhost:8080/login",
+        data: JSON.parse(parameters)
+    }).done(function (msg) {
+        console.log(msg);
+    })
+        .fail(function (jqXHR, textStatus, msg) {
+            alert(msg);
+    });
+}
+
 
 //////// UTILIZANDO O MÉTODO POST DO JQUERY //////
 
