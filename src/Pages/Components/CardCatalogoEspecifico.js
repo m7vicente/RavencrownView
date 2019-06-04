2a0496896bf7b55a511e30c4146c53bee225901d
Matheus Vicente <mvicente@outlook.com.br>
2019-06-04 19:34:53 -0300

    Alguns detalhes adicionados

--- src/Pages/Components/CardCatalogoEspecifico.js
+++ src/Pages/Components/CardCatalogoEspecifico.js
@@ -22,17 +22,21 @@ class CardCatalogoEspecifico extends React.Component {
 
   render() {
     return (
-        <div className="col-sm-3 ml-5 mr-5 mt-4 ">
+      <div className="col-sm-3 ml-5 mr-5 mt-4 ">
         <div className="card h-25">
           <div className="card-body" align="center">
             <div className=" w-100 border border-secondary">
               <img src={this.state.imagemBIN} />
+              <div>
+                <label>Nome: {this.state.servico.nomeServico}</label>
+              </div>
+              <div>
+                <label>{this.state.servico.descricaoServico}</label>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-
-
+      </div>
     );
   }
 }
