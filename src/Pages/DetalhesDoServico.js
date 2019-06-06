9a0fae6a6d8f23e1666cb726e3687ae6fd2e1fc9
Matheus Vicente <mvicente@outlook.com.br>
2019-06-06 17:06:14 -0300

    alguns detelhes editados

--- src/Pages/DetalhesDoServico.js
+++ src/Pages/DetalhesDoServico.js
@@ -1,71 +1,85 @@
-import React, { Component, Fragment } from 'react';
-import Navbar from '../Pages/Components/Navbar';
+import React, { Component, Fragment } from "react";
+import Navbar from "../Pages/Components/Navbar";
 
 class DetalhesDoServico extends React.Component {
+  state = {
+    servico: null
+  };
 
-	state = {
-		servico: null		
-	  };
+  constructor(servico) {
+    super();
+    this.state.state = servico;
+  }
 
-constructor(servico){
-	super();
-	this.state.state: servico;
-}
+  render() {
+    return (
+      <Fragment>
+        <Navbar />
+        <div className="content mt-4">
+          <div className="row justify-content-center">
+            <div className="col-sm-1">
+              <i className="fas fa-user-circle" style={{ fontSize: "72px" }} />
+            </div>
 
-	render(){
-	return (
-		<Fragment>
-			<Navbar />
-			<div className="content mt-4">
-				<div className="row justify-content-center">
-					<div className="col-sm-1">
-						<i className='fas fa-user-circle' style={{ fontSize: '72px' }}></i>
-					</div>
-
-					<div className="col-sm-4 justify-content-left">
-						<p className="h3">José Pereira Ermelino</p>
-						<p className="h4">Reformas de casas</p>
-					</div>
-				</div>
-				<div className="row justify-content-center mt-5">
-					<div className="col-sm-5">
-						<p className="h4 text-secondary">Descrição do serviço</p>
-						<div className="text-secondary">Lorem ipsum per enim senectus ut odio pretium consectetur conubia bibendum morbi venenatis dictum, blandit enim ipsum className litora posuere imperdiet primis rhoncus venenatis magna. himenaeos sapien praesent tortor facilisis ut, blandit massa lorem consectetur amet, vestibulum commodo aptent vivamus. est mauris nisi senectus himenaeos maecenas className vestibulum ultrices.
-						</div>
-					</div>
-				</div>
-				<div className="row justify-content-center mt-4">
-					<div className="col-sm-3 mt-2">
-						<label className="h4 text-secondary"> Local -  </label>
-						<label className="h5 text-primary">Domicilio</label>
-					</div>
-					<div className="col-sm-2">
-						<label className="h4"> R$ <span className="h1">100,20</span> </label>
-					</div>
-				</div>
-				<div className="row justify-content-center mt-4">
-					<div className="col-sm-3 mt-2">
-						<label className="h4 text-secondary"> Categoria </label>
-						<p className="h5 text-secondary"> Reformas </p>
-					</div>
-					<div className="col-sm-2">
-						<button type="button" className="btn border border-none btn-primary w-100" style={{ backgroundColor: 'rgb(185, 216, 69)' }}>Contratar</button>
-					</div>
-				</div>
-				<div className="row justify-content-center mt-3 border-top">
-					<div className="col-sm-7">
-						<label className="h5 text-secondary mt-3"> Comentários </label>
-					</div>
-				</div>
-				<div className="row justify-content-center mt-3 mb-3">
-					<div className="col-sm-7">
-						<textarea className="form-control" rows="5"></textarea>
-					</div>
-				</div>
-			</div>
-
-		</Fragment>
-	);
+            <div className="col-sm-4 justify-content-left">
+              <p className="h3">José Pereira Ermelino</p>
+              <p className="h4">Reformas de casas</p>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-5">
+            <div className="col-sm-5">
+              <p className="h4 text-secondary">Descrição do serviço</p>
+              <div className="text-secondary">
+                Lorem ipsum per enim senectus ut odio pretium consectetur
+                conubia bibendum morbi venenatis dictum, blandit enim ipsum
+                className litora posuere imperdiet primis rhoncus venenatis
+                magna. himenaeos sapien praesent tortor facilisis ut, blandit
+                massa lorem consectetur amet, vestibulum commodo aptent vivamus.
+                est mauris nisi senectus himenaeos maecenas className vestibulum
+                ultrices.
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-4">
+            <div className="col-sm-3 mt-2">
+              <label className="h4 text-secondary"> Local - </label>
+              <label className="h5 text-primary">Domicilio</label>
+            </div>
+            <div className="col-sm-2">
+              <label className="h4">
+                {" "}
+                R$ <span className="h1">100,20</span>{" "}
+              </label>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-4">
+            <div className="col-sm-3 mt-2">
+              <label className="h4 text-secondary"> Categoria </label>
+              <p className="h5 text-secondary"> Reformas </p>
+            </div>
+            <div className="col-sm-2">
+              <button
+                type="button"
+                className="btn border border-none btn-primary w-100"
+                style={{ backgroundColor: "rgb(185, 216, 69)" }}
+              >
+                Contratar
+              </button>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-3 border-top">
+            <div className="col-sm-7">
+              <label className="h5 text-secondary mt-3"> Comentários </label>
+            </div>
+          </div>
+          <div className="row justify-content-center mt-3 mb-3">
+            <div className="col-sm-7">
+              <textarea className="form-control" rows="5" />
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
   }
 }
 
