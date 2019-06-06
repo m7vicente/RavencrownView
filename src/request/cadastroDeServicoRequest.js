3229948aba77685e8db2ee43b24939d569f73b17
Matheus Vicente <mvicente@outlook.com.br>
2019-06-06 20:00:55 -0300

    chamada para microserviço de importação de arquivo adicionado

--- src/request/cadastroDeServicoRequest.js
+++ src/request/cadastroDeServicoRequest.js
@@ -13,34 +13,38 @@ function cadastroDeServicoRequest() {
   var estado;
   var id_Endereco;
 
-if($("#casa").is(":checked")){
+  if ($("#casa").is(":checked")) {
     rua = JSON.parse(sessionStorage.getItem("login")).endereco.rua;
     cep = JSON.parse(sessionStorage.getItem("login")).endereco.cep;
-    complemento = JSON.parse(sessionStorage.getItem("login")).endereco.complemento;
+    complemento = JSON.parse(sessionStorage.getItem("login")).endereco
+      .complemento;
     numero = JSON.parse(sessionStorage.getItem("login")).endereco.numero;
-    id_Endereco = JSON.parse(sessionStorage.getItem("login")).endereco.id_Endereco;
-    referencia = JSON.parse(sessionStorage.getItem("login")).endereco.referencia;
+    id_Endereco = JSON.parse(sessionStorage.getItem("login")).endereco
+      .id_Endereco;
+    referencia = JSON.parse(sessionStorage.getItem("login")).endereco
+      .referencia;
     bairro = JSON.parse(sessionStorage.getItem("login")).endereco.bairro;
     cidade = JSON.parse(sessionStorage.getItem("login")).endereco.cidade;
     estado = JSON.parse(sessionStorage.getItem("login")).endereco.estado;
-}
-else{
-  rua = document.getElementById("rua").value.toString();
-  cep = document.getElementById("cep").value.toString();
-  complemento = document.getElementById("complemento").value.toString();
-  numero = document.getElementById("numero").value.toString();
-  referencia = document.getElementById("referencia").value.toString();
-  bairro = document.getElementById("bairro").value.toString();
-  cidade = document.getElementById("cidade").value.toString();
-  estado = document.getElementById("estado").value.toString();
-  id_Endereco = null;
-}    
+  } else {
+    rua = document.getElementById("rua").value.toString();
+    cep = document.getElementById("cep").value.toString();
+    complemento = document.getElementById("complemento").value.toString();
+    numero = document.getElementById("numero").value.toString();
+    referencia = document.getElementById("referencia").value.toString();
+    bairro = document.getElementById("bairro").value.toString();
+    cidade = document.getElementById("cidade").value.toString();
+    estado = document.getElementById("estado").value.toString();
+    id_Endereco = null;
+  }
   const nomeServico = document.getElementById("nomeServico").value.toString();
-  const descricaoServico = document.getElementById("descricaoServico").value.toString();
-  const a = document.getElementById("precoServico").value
+  const descricaoServico = document
+    .getElementById("descricaoServico")
+    .value.toString();
+  const a = document.getElementById("precoServico").value;
   const precoServico = parseFloat(a);
   const idUsuario = JSON.parse(sessionStorage.getItem("login")).Id_Usuario;
-  const idCategoria= $("#categoria option:selected").val();
+  const idCategoria = $("#categoria option:selected").val();
 
   const parameters = JSON.stringify({
     idServico: null,
@@ -66,7 +70,7 @@ else{
     localizacaoFixa: "true"
   });
 
-console.log(parameters)
+  console.log(parameters);
 
   $.ajax({
     type: "post",
@@ -76,13 +80,47 @@ console.log(parameters)
     data: parameters
   })
 
-    .done(function (msg) {
-      $(function () {
-        $('#mdl').trigger('click'); })
+    .done(function(msg) {
+      if (document.getElementById("iptRecomendacao").files[0] != null) {
+        inserirCSV();
+      }
+
+      $(function() {
+        $("#mdl").trigger("click");
+      });
       //document.getElementById("mdl").click();
     })
 
-    .fail(function (jqXHR, textStatus, msg) {
+    .fail(function(jqXHR, textStatus, msg) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(msg);
+    });
+}
+
+function inserirCSV() {
+  const fileURL = document.getElementById("iptRecomendacao").files[0];
+
+  var form = new FormData();
+  form.append("file", fileURL);
+  form.append("id", JSON.parse(sessionStorage.getItem("login")).Id_Usuario);
+
+  $.ajax({
+    async: false,
+    crossDomain: true,
+    url: "http://localhost:8080/",
+    method: "POST",
+    processData: false,
+    contentType: false,
+    mimeType: "multipart/form-data",
+    data: form
+  })
+
+    .done(function(msg) {
+      console.log(msg);
+    })
+
+    .fail(function(jqXHR, textStatus, msg) {
       console.log(jqXHR);
       console.log(textStatus);
       console.log(msg);
@@ -90,5 +128,3 @@ console.log(parameters)
 }
 
 export default cadastroDeServicoRequest;
-
-
