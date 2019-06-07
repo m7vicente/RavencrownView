2c8dbebd25a9b1ca19457ec270f9c638ea07feee
VicTenorio <vitoriabasto17@gmail.com>
2019-06-06 21:27:33 -0300

    contrato request

--- src/Pages/Components/nightMode.js
+++ src/Pages/Components/nightMode.js
@@ -12,17 +12,7 @@ function NightMode(){
         document.getElementById("domesticos").setAttribute("class", "font");
         document.getElementById("configuracoes").setAttribute("class", "font");
         document.getElementById("vestuario").setAttribute("class", "font");
-    } else {
-        document.getElementById("configuracoes").setAttribute("class", "font");
-        document.getElementById("aulas").removeAttribute("class", "font");
-        document.getElementById("auto").removeAttribute("class", "font");
-        document.getElementById("code").removeAttribute("class", "font");
-        document.getElementById("reformas").removeAttribute("class", "font");
-        document.getElementById("culinaria").removeAttribute("class", "font");
-        document.getElementById("domesticos").removeAttribute("class", "font");
-        document.getElementById("configuracoes").removeAttribute("class", "font");
-        document.getElementById("vestuario").removeAttribute("class", "font");
-    }
+    } 
 }
 
 export default NightMode;
