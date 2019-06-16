48d2642af1e3872ce1f0bcc803f8b57128f54228
Matheus Vicente <mvicente@outlook.com.br>
2019-06-16 12:09:34 -0300

    Alteração na habilitação na validação da data escolhida

--- src/request/DatasRequest.jsx
+++ src/request/DatasRequest.jsx
@@ -1,58 +1,56 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 function ValidaDataEscolhida(Data, IdServico, buttom) {
+  var disponivel = false;
 
-    var disponivel = false;
+  const parameters = JSON.stringify({
+    idDataAgendamento: null,
+    idPrestador: null,
+    idServico: IdServico,
+    idContrato: null,
+    idConsumidor: null,
+    idDemanda: null,
+    dtAgendamento: Data,
+    tipoReserva: "T"
+  });
 
-    const parameters = JSON.stringify({
-        "idDataAgendamento": null,
-        "idPrestador": null,
-        "idServico": IdServico,
-        "idContrato": null,
-        "idConsumidor": null,
-        "idDemanda": null,
-        "dtAgendamento": Data,
-        "tipoReserva": "T"
-    });
+  const response = $.ajax({
+    type: "post",
+    contentType: "application/json;charset=UTF-8",
+    url: "https://ravencrownservice.azurewebsites.net/Datas/Validar",
+    async: true,
+    data: parameters
+  })
+    .done(function(msg) {
+      $(function() {
+        if (msg == "Disponivel") {
+          disponivel = true;
+          buttom.removeAttribute("disabled");
+        } else {
+          alert("Data Indisponivel");
+          buttom.setAttribute("disabled", "disabled");
+        }
+      });
+    })
+    .fail(function(jqXHR, textStatus, msg) {
+      console.log(jqXHR);
+      console.log(textStatus);
+      console.log(msg);
+    }).responseJSON;
 
-    const response = (
-        $.ajax({
-            type: "post",
-            contentType: "application/json;charset=UTF-8",
-            url: "https://ravencrownservice.azurewebsites.net/Datas/Validar",
-            async: false,
-            data: parameters
-        }).done(function (msg) {
-            $(function () {
-                if (msg == "Disponivel") {
-                    disponivel = true;
-                    buttom.removeAttribute("disabled");
-                }else{
-                    alert("Data Indisponivel");
-                    buttom.setAttribute("disabled", "disabled");
-                }
-            })
-        }).fail(function (jqXHR, textStatus, msg) {
-            console.log(jqXHR);
-            console.log(textStatus);
-            console.log(msg);
-        })
-    ).responseJSON;
+  console.log(parameters);
+  console.log(response);
 
-    console.log(parameters);
-    console.log(response);    
-
-    return disponivel
+  return disponivel;
 }
 
 function sleep(milliseconds) {
-    var start = new Date().getTime();
-    for (var i = 0; i < 1e7; i++) {
-      if ((new Date().getTime() - start) > milliseconds){
-        break;
-      }
-    }  
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if (new Date().getTime() - start > milliseconds) {
+      break;
+    }
+  }
 }
-
 
 export default ValidaDataEscolhida;
