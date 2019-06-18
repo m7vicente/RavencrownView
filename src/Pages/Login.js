87d4c82066f2c487e2395c87c0b805d1a10e0892
henrique-gs <henriquegodez@gmail.com>
2019-06-17 21:21:13 -0300

    Alterações na tela de login

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -15,12 +15,11 @@ class Login extends React.Component {
             alert("Usuario ou senha invalidos");
         }
     }
-    
+
     render() {
         return (
-            <div className="bg-info">
-                <div className="row h-100 p-5 bg-info">
-                <div className="col bg-info mb-2 mt-5">
+            <div className="row h-100 p-5 bg-info">
+                <div className="col bg-info h-100 mb-2 mt-5">
                     <div className="row bg-info mt-5 mb-5 justify-content-center">
                         <div className="justify-content-left container containerLogin mt-5 mb-5 w-25 h-75 bg-white border border-secondary" style={{ borderRadius: '2%' }}>
                             <div className="row justify-content-center mt-3">
@@ -30,17 +29,23 @@ class Login extends React.Component {
                             </div>
                             <div className="row justify-content-center mt-5 h-100">
                                 <div className="col mt-5">
-                                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label ">
-                                        <input id="txtEmail" align="center" className="input border border-white mdl-textfield__input" type="text" />
-                                        <label className="mdl-textfield__label">E-mail</label>
+                                    <div className="mdl-textfield mdl-js-textfield">
+                                        <i className="material-icons" style={{ fontSize: '30px' }}>
+                                            perm_identity
+                                        </i>
+                                        <input id="txtEmail" align="center" className="input w-100 border-top-0 border-left-0 border-right-0" type="text" placeholder="E-mail" />
                                     </div>
                                 </div>
                             </div>
                             <div className="row justify-content-center mt-2 h-100">
-                                <div className="col">
-                                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                        <input id="txtSenha" className="input border border-white mdl-textfield__input" type="password" style={{ height: '25%;' }} />
-                                        <label className="mdl-textfield__label" for="sample3">Senha</label>
+                                <div className="">
+                                    <i class="material-icons" style={{ fontSize: '30px' }}>
+                                        vpn_key
+                                    </i>
+                                </div>
+                                <div className="">
+                                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label" style={{ marginTop: "-10%" }}>
+                                        <input id="txtSenha" className="input w-100 border-top-0 border-left-0 border-right-0" type="password" placeholder="Senha" style={{ height: '25%;' }} />
                                     </div>
                                 </div>
                             </div>
@@ -51,7 +56,7 @@ class Login extends React.Component {
                                 <Link to="/">
                                     <button
                                         id="botao"
-                                        type="button"                                       
+                                        type="button"
                                         onClick={this.doLogin}
                                         onClick={request}
                                         style={{ fontSize: "150%;" }}
@@ -66,10 +71,9 @@ class Login extends React.Component {
                             </div>
                         </div>
                     </div>
-                </div>      
+                </div>
             </div>
-            </div>
-            
+
         );
     }
 }
